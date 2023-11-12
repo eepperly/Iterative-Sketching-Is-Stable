d96f3426fb8798c1211ca5e97cfcecdec3b96c71
Ethan Epperly <ethanepperly@Ethans-MBP.lan>
2023-11-12 10:42:01 -0800

    Improve rng efficiency of sparsesign

--- code/sparsesign.c
+++ code/sparsesign.c
@@ -3,6 +3,15 @@
 
 #define RADEMACHER() (2 * (rand() % 2) -1)
 
+int log_base_d(long a, int d) {
+    int output = 0;
+    while (a > 0) {
+        a /= d;
+        output += 1;
+    }
+    return output-1;
+}
+
 void mexFunction(
 		 int nlhs,       mxArray *plhs[],
 		 int nrhs, const mxArray *prhs[]
@@ -42,19 +51,32 @@ void mexFunction(
 	mwSize zeta = (mwSize) mxGetScalar(prhs[2]);
 	mwSize nnz = m*zeta;
 
+    int idx_per_rand = log_base_d((long) RAND_MAX + 1, d);
+    int bit_per_rand = log_base_d((long) RAND_MAX + 1, 2);
+
 	if (zeta > d) zeta = d;
 
-	double val = 1/sqrt((double) zeta);
+	double lowval = -idx_per_rand;// -1/sqrt((double) zeta);
+    double increment = -2*lowval;
 
-        plhs[0] = mxCreateSparse(d,m,nnz,false);
-        double *vals  = mxGetPr(plhs[0]);
-        mwIndex *rows = mxGetIr(plhs[0]);
-        mwIndex *colstarts = mxGetJc(plhs[0]);
+    plhs[0] = mxCreateSparse(d,m,nnz,false);
+    double *vals  = mxGetPr(plhs[0]);
+    mwIndex *rows = mxGetIr(plhs[0]);
+    mwIndex *colstarts = mxGetJc(plhs[0]);
 
 	// Set values
-	for (int i = 0; i < nnz; ++i) {
-	    vals[i] = RADEMACHER() * val;
+    int myrand = rand();
+	for (int i = 0; i+bit_per_rand < nnz; i += bit_per_rand) {
+        for (int j = i; j < i+bit_per_rand; ++j) {
+	        vals[j] = (myrand % 2) * increment + lowval;
+            myrand = myrand >> 1;
+        }
+        myrand = rand();
 	}
+    for (int i = bit_per_rand*(nnz/bit_per_rand); i < nnz; ++i) {
+        vals[i] = (myrand % 2) * increment + lowval;
+        myrand = myrand >> 1;
+    }
 
 	// Set column starts
 	for (int i = 1; i < m+1; ++i){
@@ -62,16 +84,24 @@ void mexFunction(
 	}
 
 	// Set row indices
-        for (int i = 0; i < m*zeta; i += zeta) {
-	    int idx = 1;
-	    rows[i] = rand() % d;
+    myrand = rand();
+    int ir = 0;
+    for (int i = 0; i < m*zeta; i += zeta) {
+	    int idx = 0;
 	    while (idx < zeta) {
-		rows[i+idx] = rand() % d;
-		int j = 0;
-		for (; j < idx; ++j) {
-		    if (rows[i+idx] == rows[i+j]) break;
-		}
-		idx += (int) (j == idx);
+		    rows[i+idx] = myrand % d;
+            ir++;
+            if (ir == idx_per_rand) {
+                ir = 0;
+                myrand = rand();
+            } else {
+                myrand /= d;
+            }
+		    int j = 0;
+		    for (; j < idx; ++j) {
+		        if (rows[i+idx] == rows[i+j]) break;
+		    }
+		    idx += (int) (j == idx);
 	    }
 	}
     #endif
